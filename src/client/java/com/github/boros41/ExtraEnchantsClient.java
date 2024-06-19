407ee88d0db9f496822062d27be04cbca1cae36c
Boros <kevstar421@gmail.com>
2024-06-19 01:56:00 -0400

    Initial commit

---
+++ src/client/java/com/github/boros41/ExtraEnchantsClient.java
@@ -0,0 +1,10 @@
+package com.github.boros41;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraEnchantsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/github/boros41/ExtraEnchants.java
@@ -0,0 +1,33 @@
+package com.github.boros41;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.minecraft.registry.Registries;
+import net.minecraft.item.Item;
+
+public class ExtraEnchants implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "extra-enchants";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final Item STRENGTH_SCROLL =
+			Registry.register(
+					Registries.ITEM,
+					new Identifier("scroll", "strength_scroll"),
+					new Item(new Item.Settings()));
+
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		LOGGER.info("Hello Fabric world!");
+	}
+}
