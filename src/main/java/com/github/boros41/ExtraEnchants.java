92385ae9c22b9cc7c16e48dbf14f07d90401afe3
Boros <kevstar421@gmail.com>
2024-06-21 01:20:20 -0400

    Added ability to consume strength scroll and give the player strength 4.

--- src/main/java/com/github/boros41/ExtraEnchants.java
+++ src/main/java/com/github/boros41/ExtraEnchants.java
@@ -1,31 +1,16 @@
 package com.github.boros41;
 
+import com.github.boros41.scrolls.StrengthScroll;
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import net.minecraft.registry.Registries;
-import net.minecraft.item.Item;
 
 public class ExtraEnchants implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "extra-enchants";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	public static final Item STRENGTH_SCROLL =
-			Registry.register(
-					Registries.ITEM,
-					new Identifier(MOD_ID, "strength_scroll"),
-					new Item(new Item.Settings()));
-
-
 
 	@Override
 	public void onInitialize() {
@@ -35,7 +20,7 @@ public class ExtraEnchants implements ModInitializer {
 		LOGGER.info("Registering Mod Items for " + MOD_ID);
 
 		// register strength_scroll item in the ingredients creative tab after the enchanted books
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> content.addAfter(Items.ENCHANTED_BOOK, STRENGTH_SCROLL));
-		LOGGER.info("Successfully registered strength_scroll");
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> content.addAfter(Items.ENCHANTED_BOOK, StrengthScroll.STRENGTH_SCROLL));
+		LOGGER.info("Successfully registered " + StrengthScroll.STRENGTH_SCROLL + " to creative ingredients tab");
 	}
 }
---
+++ src/main/java/com/github/boros41/scrolls/StrengthScroll.java
@@ -0,0 +1,44 @@
+package com.github.boros41.scrolls;
+
+import com.github.boros41.ExtraEnchants;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class StrengthScroll extends Item {
+    public static final Item STRENGTH_SCROLL =
+            Registry.register(
+                    Registries.ITEM,
+                    new Identifier(ExtraEnchants.MOD_ID, "strength_scroll"),
+                    new StrengthScroll(new Item.Settings()));
+
+    public StrengthScroll(Item.Settings settings) {
+        super (settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(SoundEvents.BLOCK_FIRE_EXTINGUISH, 1.0F, 1.0F);
+        user.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 1000, 3)); // strength 4 for 50 seconds
+        ExtraEnchants.LOGGER.info("Successfully applied strength status effect to " + user);
+
+        // strength scroll was used so remove one from the player's item stack and consume it
+        ItemStack itemStack = user.getStackInHand(hand);
+        itemStack.decrement(1);
+        return TypedActionResult.consume(itemStack);
+    }
+
+    @Override
+    public String toString() {
+        return "Strength Scroll";
+    }
+}
